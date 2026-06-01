bc261b928943266e0679bda5838f90146a373c58
ChanceCtrl <142356358+ChanceCtrl@users.noreply.github.com>
2026-05-31 21:02:11 -0400

    working dash emulator

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,20 @@ mod tasks;
 use embassy_executor::Spawner;
 use embassy_rp::adc::{self, Adc, Channel};
 use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{Input, Level, Output, Pull};
-use embassy_rp::i2c::{I2c};
-use embassy_rp::peripherals::{USB};
-use embassy_rp::spi::{Config, Spi};
-use embassy_rp::usb::{self, Driver};
-use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use embassy_sync::signal::Signal;
+use embassy_rp::gpio::{Input, Pin, Pull};
+use embassy_rp::{
+    gpio::{Level, Output},
+    peripherals::USB,
+    spi::{Config, Spi},
+    usb::{Driver, InterruptHandler},
+};
 use embassy_time::Delay;
 use embassy_time::Timer;
 use embassy_usb_logger;
-use hx711_spi::Hx711;
 use log::info;
+use mcp2518fd::settings::{
+    BitTimeConfiguration, DataBitTimeConfiguration, NominalBitTimeConfiguration,
+};
 use mcp2518fd::{
     id::{ExtendedId, Id},
     memory::controller::{
@@ -34,10 +36,6 @@ use mcp2518fd::{
     },
     spi::MCP2518FD,
 };
-use mlx9064x::Mlx90640Driver;
-
-use crate::tasks::tire_temp::tire_temp_task;
-use crate::tasks::wheel_speed::wheel_speed_task;
 
 use {defmt_rtt as _, panic_probe as _};
 
@@ -59,17 +57,7 @@ async fn main(_spawner: Spawner) {
     Timer::after_secs(2).await;
     info!("Hello World!");
 
-    let wheel_speed = Input::new(p.PIN_29, Pull::None);
-    static SPEED: Signal<CriticalSectionRawMutex, u16> = Signal::new();
-    let _ = _spawner.spawn(wheel_speed_task(wheel_speed, &SPEED));
-
-    let mut adc = Adc::new(p.ADC, Irqs, adc::Config::default());
-    let mut shockpot = Channel::new_pin(p.PIN_26, Pull::None);
-
-    // let i2c0 = I2c::new_blocking(p.I2C0, p.PIN_25, p.PIN_24, embassy_rp::i2c::Config::default());
-    // let cam = Mlx90640Driver::new(i2c0, 0x33).unwrap();
-    // static TEMP: Signal<CriticalSectionRawMutex, f32> = Signal::new();
-    // let _ = _spawner.spawn(tire_temp_task(cam, &TEMP));
+    let btn_5 = Input::new(p.PIN_25, Pull::Up);
 
     let can_miso = p.PIN_0;
     let can_mosi = p.PIN_3;
@@ -94,6 +82,13 @@ async fn main(_spawner: Spawner) {
     can.configure(Settings::default(), &mut Delay)
         .await
         .expect("Failed to configure MCP2518");
+
+    can.configure_bit_timing(BitTimeConfiguration {
+        nominal: NominalBitTimeConfiguration::RATE_500_KBIT,
+        data: DataBitTimeConfiguration::RATE_500_KBIT,
+    })
+    .await
+    .expect("Failed to set CAN baudrate");
 
     // Configure FIFO 1 as an RX FIFO to hold up to 16 messages with a max
     // payload size of 64 bytes
@@ -127,21 +122,19 @@ async fn main(_spawner: Spawner) {
         .await
         .expect("Failed to change chip operating mode");
 
-
-    // let spi1 = Spi::new(p.SPI1, p.PIN_14, p.PIN_27, p.PIN_28, p.DMA_CH2, p.DMA_CH3, Config::default());
-    // let mut hx711 = Hx711::new(spi1);
-
-    // hx711.reset_async().await.unwrap();
-    // hx711.set_mode_async(hx711_spi::Mode::ChAGain64).await.unwrap();
-
-    // let mut counter = 0;
-
     loop {
-        // let force = (hx711.read_async().await.unwrap() >> 9) + 40;
-        // let _ = can.tx_queue_transmit_message(&TxMessage::from_frame(messages::LoadCellFrame::new(force).unwrap()).unwrap()).await;
-
-        let shockpot_reading = adc.read(&mut shockpot).await.unwrap_or_default();
-        let _ = can.tx_queue_transmit_message(&TxMessage::from_frame(messages::ShockpotFrame::new(shockpot_reading).unwrap()).unwrap()).await;
+        let message = TxMessage::from_frame(
+            ksu_rs_dbc::messages::DashButtons::new(
+                false,
+                btn_5.is_low(),
+                false,
+                false,
+                false,
+                false,
+            )
+            .unwrap(),
+        )
+        .unwrap();
 
         // if temp.signaled() {
         //     let _ = can.tx_queue_transmit_message(&TxMessage::from_frame(messages::TiretempFrame::new(temp.try_take().unwrap()).unwrap()).unwrap()).await;
@@ -152,13 +145,6 @@ async fn main(_spawner: Spawner) {
         }
         // Send a message with the TXQ
 
-        // Read the message back (we are in loopback mode)
-        // match can.rx_fifo_get_next(FifoNumber::Fifo1).await {
-        //     Ok(Some(frame)) => info!("Received frame {:?}", frame),
-        //     Ok(None) => info!("No message to read!"),
-        //     Err(e) => info!("Error reading from FIFO: {:?}", e),
-        // }
-
-        Timer::after_millis(1).await;
+        Timer::after_millis(1000).await;
     }
 }
