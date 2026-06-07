f2b0ad79c46fe1edde8d9217506a182e89de2296
ChanceCtrl <142356358+ChanceCtrl@users.noreply.github.com>
2026-06-07 05:10:34 -0400

    fucking robots
    fuck this
    
    mrow
    
    fuck you
    
    cunt
    
    what
    
    unt
    
    fuck you
    
    cunt

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,27 @@
 #![no_std]
 #![no_main]
 
-mod messages;
-mod tasks;
-
-// use defmt::info;
 use embassy_executor::Spawner;
 use embassy_rp::{
     bind_interrupts,
-    gpio::{Level, Output},
     peripherals::{self, USB},
-    spi::{self, Spi},
     uart::{self, Async, Uart},
     usb::{self, Driver},
 };
-use embassy_time::{Delay, Timer};
+use embassy_time::Timer;
 use embassy_usb_logger;
-use log::info;
-use mcp2518fd::{
-    id::{ExtendedId, Id},
-    memory::controller::{
-        configuration::OperationMode,
-        fifo::{FifoNumber, PayloadSize},
-        filter::FilterNumber,
-    },
-    message::tx::TxMessage,
-    settings::{
-        BitTimeConfiguration, DataBitTimeConfiguration, FifoConfiguration, FifoMode,
-        FilterConfiguration, FilterMatchMode, NominalBitTimeConfiguration, RxFifoConfiguration,
-        Settings,
-    },
-    spi::MCP2518FD,
-};
-
-use crate::vn_rs::VectorNav;
-// use crate::{can_helpers::set_joe_can, vn_rs::VectorNav};
+use log::{error, info};
 
 use {defmt_rtt as _, panic_probe as _};
 
-mod can_helpers;
 mod vn_rs;
+
+use vn_rs::VectorNavData;
 
 bind_interrupts!(struct UsbIrqs {
     USBCTRL_IRQ => usb::InterruptHandler<USB>;
 });
+
 bind_interrupts!(struct UartIrqs {
     UART1_IRQ => uart::InterruptHandler<peripherals::UART1>;
 });
@@ -53,104 +31,38 @@ async fn logger_task(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(4096, log::LevelFilter::Info, driver);
 }
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    // Get our peripherals ready
-    let pins = embassy_rp::init(Default::default());
-
-    // Start serial print type shi
-    let driver = Driver::new(pins.USB, UsbIrqs);
-    let _ = _spawner.spawn(logger_task(driver)).unwrap();
-
-    Timer::after_secs(2).await;
-    info!("Hello World!");
-
-    // Setup a SPI for the CAN Controller
-    // let spi0 = Spi::new(
-    //     pins.SPI0,
-    //     pins.PIN_2,
-    //     pins.PIN_3,
-    //     pins.PIN_0,
-    //     pins.DMA_CH0,
-    //     pins.DMA_CH1,
-    //     spi::Config::default(),
-    // );
-    // let mut can = MCP2518FD::new(spi0, Output::new(pins.PIN_1, Level::High));
-    //
-    // set_joe_can(can);
-
-    // Setup a UART for the VN
-    let mut uart_config_tech = uart::Config::default();
-    uart_config_tech.baudrate = 230_400;
-
-    let mut uart_controller = Uart::new(
-        pins.UART1,
-        pins.PIN_24,  // Board TX, VN RX
-        pins.PIN_25,  // Board RX, VN TX
-        UartIrqs,     // Given by the bind_interrupts! macro above
-        pins.DMA_CH2, // Unused DMA channels?
-        pins.DMA_CH3,
-        uart_config_tech, // Adjusted baudrate
-    );
-
-    // info!("uart_controller return: {:?}", uart_controller.);
-
-    info!(
-        "Spawner return: {:?}",
-        _spawner.spawn(uart_task(uart_controller))
-    );
-
-    loop {}
-}
-
 #[embassy_executor::task]
-async fn uart_task(mut uart_controller: Uart<'static, Async>) {
-    let vn = VectorNav::new();
+async fn uart_task(mut uart: Uart<'static, Async>) {
+    let mut vn = VectorNavData::new();
 
     let mut rx_buf = [0u8; 64];
 
     loop {
-        let read_res = uart_controller.read(&mut rx_buf).await;
-
-        if vn.check_sync_byte(&rx_buf) && read_res == Ok(()) {
-            info!("{:?}: {:?}", rx_buf, read_res);
+        let read_res = uart.read(&mut rx_buf).await;
+        if read_res.is_err() {
+            info!("{:?}", read_res);
+            continue;
         }
 
-        info!("At bin check");
-        match vn.check_bin(bin_buffer) {
-            Ok(vn_rs::VectorNavBin::Bin20hz) => {
-                unsafe {
-                    if uart_controller
-                        .read(&mut vn.bin1_data.buffer)
-                        .await
-                        .is_err()
-                    {
-                        continue;
-                    };
-                }
-
-                if vn.check_values(vn_rs::VectorNavBin::Bin20hz).is_err() {
-                    continue;
-                };
-            }
-            Ok(vn_rs::VectorNavBin::Bin400hz) => {
-                unsafe {
-                    if uart_controller
-                        .read(&mut vn.bin2_data.buffer)
-                        .await
-                        .is_err()
-                    {
-                        continue;
-                    };
-                }
-
-                if vn.check_values(vn_rs::VectorNavBin::Bin400hz).is_err() {
-                    continue;
-                };
-            }
-            Err(_) => {
-                continue;
-            }
-        };
+        vn.update(rx_buf);
     }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let driver = Driver::new(p.USB, UsbIrqs);
+    spawner.spawn(logger_task(driver)).unwrap();
+
+    let mut uart_cfg = uart::Config::default();
+    uart_cfg.baudrate = 230_400;
+
+    let uart = Uart::new(
+        p.UART1, p.PIN_24, p.PIN_25, UartIrqs, p.DMA_CH2, p.DMA_CH3, uart_cfg,
+    );
+
+    spawner.spawn(uart_task(uart)).unwrap();
+
+    loop {}
 }
--- src/vn_rs.rs
+++ src/vn_rs.rs
@@ -1,17 +1,35 @@
 use log::info;
+use zerocopy::TryFromBytes;
 
+#[derive(Clone, Copy, Debug)]
 pub enum VectorNavError {
     BadCRC,
     NoBinFound,
 }
-pub enum VectorNavBin {
-    Bin20hz,
-    Bin400hz,
+
+enum VectorNavBin {
+    Bin20Hz,
+    Bin400Hz,
 }
 
-#[repr(C)]
-#[derive(Debug, Copy, Clone, Default)]
-pub struct Bin20hzType {
+impl VectorNavBin {
+    const fn id(&self) -> [u8; 2] {
+        match self {
+            VectorNavBin::Bin20Hz => [0x42, 0x10],
+            VectorNavBin::Bin400Hz => [0xA8, 0x01],
+        }
+    }
+
+    const fn size(&self) -> usize {
+        match self {
+            VectorNavBin::Bin20Hz => 36,
+            VectorNavBin::Bin400Hz => 50,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default, TryFromBytes)]
+struct Bin20Hz {
     pub time: u64, // 8
 
     pub latitude: f64,  // 16
@@ -23,11 +41,10 @@ pub struct Bin20hzType {
     pub checksum: u16, // 36
 }
 
-#[repr(C)]
-#[derive(Debug, Copy, Clone, Default)]
-pub struct Bin400hzType {
+#[derive(Clone, Copy, Debug, Default, TryFromBytes)]
+struct Bin400hz {
     pub yaw: f32,   // 4
-    pub pitch: f32, //8
+    pub pitch: f32, // 8
     pub roll: f32,  // 12
 
     pub w_x: f32, // 16
@@ -45,21 +62,11 @@ pub struct Bin400hzType {
     pub checksum: u16, // 50
 }
 
-#[repr(C)]
-pub union Bin20hzUnion {
-    pub buffer: [u8; 36],
-    pub data: Bin20hzType,
-}
-
-#[repr(C)]
-pub union Bin400hzUnion {
-    pub buffer: [u8; 50],
-    pub data: Bin400hzType,
-}
-
-const MAX_PACKET_SIZE: usize = size_of::<Bin400hzUnion>();
 enum ParseState {
-    WaitingForSync,
+    WaitingForSync {
+        buffer: [u8; 2],
+        received: usize,
+    },
     ReadingBin {
         buffer: [u8; 2],
         received: usize,
@@ -68,168 +75,106 @@ enum ParseState {
         bin: VectorNavBin,
         len: usize,
         received: usize,
-        buffer: [u8; MAX_PACKET_SIZE],
+        buffer: [u8; 50],
     },
 }
 
-pub struct VectorNav {
-    // uart_controller: Uart<'d, Async>,
-    // can_controller: MCP2518FD<SPI, CS>,
-    pub bin1_data: Bin20hzUnion,
-    pub bin2_data: Bin400hzUnion,
+pub struct VectorNavData {
+    bin_20hz: Bin20Hz,
+    bin_400hz: Bin400hz,
     state: ParseState,
 }
 
-// impl<SPI, CS> VectorNav {
-impl VectorNav {
-    pub fn new(// uart_controller: Uart<'d, Async>,
-        // can_controller: MCP2518FD<SPI, CS>,
-    ) -> VectorNav {
-        info!("Initializing VectorNav...");
-
-        // TODO: Setup VN message bins and other params every boot to prevent fuckery from people
-
-        return VectorNav {
-            // uart_controller,
-            // can_controller,
-            bin1_data: Bin20hzUnion {
-                data: Bin20hzType::default(),
+impl VectorNavData {
+    pub fn new() -> VectorNavData {
+        VectorNavData {
+            bin_20hz: Bin20Hz::default(),
+            bin_400hz: Bin400hz::default(),
+            state: ParseState::WaitingForSync {
+                buffer: [0_u8; 2],
+                received: 0,
             },
-            bin2_data: Bin400hzUnion {
-                data: Bin400hzType::default(),
-            },
-            state: ParseState::WaitingForSync,
-        };
+        }
     }
 
-    pub fn update(&mut self, buffer: &[u8]) -> Result<VectorNavBin, VectorNavError> {
-        for &byte in buffer.iter() {
+    pub fn update(&mut self, input: [u8; 64]) {
+        for byte in input.iter() {
             match &mut self.state {
-                // Check if we have a sync byte
-                ParseState::WaitingForSync => {
-                    if byte == 0xFA {
-                        self.state = ParseState::ReadingBin {
-                            buffer: [0; 2],
-                            received: 0,
-                        }
+                ParseState::WaitingForSync { buffer, received } => {
+                    buffer[*received] = *byte;
+                    *received += 1;
+
+                    if *received == 2 {
+                        match *buffer {
+                            [0xFA, 0x01] => {
+                                self.state = ParseState::ReadingBin {
+                                    buffer: [0_u8; 2],
+                                    received: 0,
+                                }
+                            }
+
+                            _ => {
+                                self.state = ParseState::WaitingForSync {
+                                    buffer: [0_u8; 2],
+                                    received: 0,
+                                };
+                            }
+                        };
                     }
                 }
 
-                // Figure out which bin it is
                 ParseState::ReadingBin { buffer, received } => {
-                    buffer[*received] = byte;
+                    buffer[*received] = *byte;
                     *received += 1;
 
                     if *received == 2 {
                         match *buffer {
                             [0x42, 0x10] => {
                                 self.state = ParseState::ReadingPacket {
-                                    bin: VectorNavBin::Bin20hz,
-                                    len: size_of::<Bin20hzUnion>(),
+                                    bin: VectorNavBin::Bin20Hz,
+                                    len: VectorNavBin::Bin20Hz.size(),
                                     received: 0,
-                                    buffer: [0; MAX_PACKET_SIZE],
-                                };
+                                    buffer: [0_u8; 50],
+                                }
                             }
                             [0xA8, 0x01] => {
                                 self.state = ParseState::ReadingPacket {
-                                    bin: VectorNavBin::Bin20hz,
-                                    len: size_of::<Bin400hzUnion>(),
+                                    bin: VectorNavBin::Bin400Hz,
+                                    len: VectorNavBin::Bin400Hz.size(),
                                     received: 0,
-                                    buffer: [0; MAX_PACKET_SIZE],
-                                };
+                                    buffer: [0_u8; 50],
+                                }
                             }
                             _ => {
-                                self.state = ParseState::WaitingForSync;
-                                return Err(VectorNavError::NoBinFound);
+                                self.state = ParseState::WaitingForSync {
+                                    buffer: [0_u8; 2],
+                                    received: 0,
+                                };
                             }
                         };
                     }
                 }
 
-                // Read the packet out
                 ParseState::ReadingPacket {
-                    buffer,
-                    received,
-                    len,
                     bin,
-                } => {
-                    buffer[*received] = byte;
-                    *received += 1;
-
-                    if *received == *len {
-                        self.check_values(bin);
-
-                        self.state = ParseState::WaitingForSync;
-                    }
-                }
+                    len,
+                    received,
+                    buffer,
+                } => {}
             }
         }
-
-        Err(VectorNavError::NoBinFound)
     }
 
-    pub fn check_bin(&self, buffer: &[u8]) -> Result<VectorNavBin, VectorNavError> {
-        if buffer[0] == 0x42 && buffer[1] == 0x10 {
-            Ok(VectorNavBin::Bin20hz)
-        } else if buffer[0] == 0xA8 && buffer[1] == 0x01 {
-            Ok(VectorNavBin::Bin400hz)
-        } else {
-            Err(VectorNavError::NoBinFound)
-        }
-    }
-
-    pub fn check_values(&self, bin: &VectorNavBin) -> Result<VectorNavBin, VectorNavError> {
-        let crc = self.calc_crc(&bin);
-
-        match bin {
-            VectorNavBin::Bin20hz => unsafe {
-                if self.bin1_data.data.checksum == crc {
-                    info!("Got good 20hz bin!");
-                    return Ok(VectorNavBin::Bin20hz);
-                } else {
-                    return Err(VectorNavError::BadCRC);
-                }
-            },
-
-            VectorNavBin::Bin400hz => unsafe {
-                if self.bin2_data.data.checksum == crc {
-                    info!("Got good 400hz bin!");
-                    return Ok(VectorNavBin::Bin400hz);
-                } else {
-                    return Err(VectorNavError::BadCRC);
-                }
-            },
-        }
-    }
-
-    fn calc_crc(&self, bin: &VectorNavBin) -> u16 {
+    fn get_crc(buffer: &[u8]) -> u16 {
         let mut crc = 0_u16;
 
-        match bin {
-            VectorNavBin::Bin20hz => {
-                unsafe {
-                    // We need all the bytes except the last 2 which is the VNs CRC
-                    for b in 0..(self.bin1_data.buffer.len() - 2) {
-                        crc = (crc >> 8) | (crc << 8); // Rotate crc left 8 bits
-                        crc ^= self.bin1_data.buffer[b] as u16; // XOR crc with data[i]
-                        crc ^= (crc & 0x00FF) >> 4; // XOR crc with lower 4 bits of crc
-                        crc ^= crc << 12; // Rotate crc left 12 bits
-                        crc ^= (crc & 0x00FF) << 5; // XOR crc w lower 8 bits & shift left 5 bits
-                    }
-                }
-            }
-            VectorNavBin::Bin400hz => {
-                unsafe {
-                    // We need all the bytes except the last 2 which is the VNs CRC
-                    for b in 0..(self.bin2_data.buffer.len() - 2) {
-                        crc = (crc >> 8) | (crc << 8); // Rotate crc left 8 bits
-                        crc ^= self.bin2_data.buffer[b] as u16; // XOR crc with data[i]
-                        crc ^= (crc & 0x00FF) >> 4; // XOR crc with lower 4 bits of crc
-                        crc ^= crc << 12; // Rotate crc left 12 bits
-                        crc ^= (crc & 0x00FF) << 5; // XOR crc w lower 8 bits & shift left 5 bits
-                    }
-                }
-            }
+        // We need all the bytes except the last 2 which is the VNs CRC
+        for b in 0..(buffer.len() - 2) {
+            crc = (crc >> 8) | (crc << 8); // Rotate crc left 8 bits
+            crc ^= buffer[b] as u16; // XOR crc with data[i]
+            crc ^= (crc & 0x00FF) >> 4; // XOR crc with lower 4 bits of crc
+            crc ^= crc << 12; // Rotate crc left 12 bits
+            crc ^= (crc & 0x00FF) << 5; // XOR crc w lower 8 bits & shift left 5 bits
         }
 
         crc
